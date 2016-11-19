f4b6173f6eff48f697d5af288b414847a21d5c20
Rohit Singh <Rohit Singh>
2016-11-19 16:58:42 +0530

    updated on 19 nov 2016

--- server.js
+++ server.js
@@ -5,13 +5,126 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+//server-side templating
+
+var articles ={
+   'article-one'  :{  //object
+    title : 'Article-one | Rohit Singh',
+    heading : 'Article-one',
+    date :  'Sep 5, 2016',
+    content : `
+    <p>
+      This is the content for my first article.This is the content for my first article.
+      This is the content for my first article.This is the content for my first article.
+      This is the content for my first article.This is the content for my first article.
+    </p>
+    <p>
+      This is the content for my first article.This is the content for my first article.
+      This is the content for my first article.This is the content for my first article.
+      This is the content for my first article.This is the content for my first article.
+    </p>
+    <p>
+      This is the content for my first article.This is the content for my first article.
+      This is the content for my first article.This is the content for my first article.
+      This is the content for my first article.This is the content for my first article.
+    </p>`
+  },
+   'article-two' : {
+     title : 'Article-two | Rohit Singh',
+     heading : 'article-two',
+     date :  'Sep 15, 2016',
+     content : `
+     <p>
+     This is the content for my second article.
+     </p>
+    `
+     },
+    'article-three' : {
+     title : 'article-three | Rohit Singh',
+     heading : 'Article-three',
+     date :  'Sep 25, 2016',
+     content : `
+     <p>
+       This is the content for my third article.
+     </p>
+    `
+   }
+ };
+
+
+function createTemplate(data) {
+  var title = data.title;
+  var date = data.date;
+  var heading = data.heading;
+  var content = data.content;
+  var htmlTemplate =
+    `<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>${title}</title>
+        <meta name="viewport" content="width-device-width,initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+      </head>
+      <body>
+        <div class="container">
+            <div class="">
+                <a href="/">Home</a>
+            </div>
+            <hr>
+            <h3>${heading}</h3>
+            <div class="">
+              ${date}
+            </div>
+            <div class="">
+              ${content}
+            </div>
+        </div>
+      </body>
+    </html>
+  `;
+  return htmlTemplate;
+}
+
+var counter =0;
+app.get('/counter',function (req,res) {
+  counter = counter +1;
+  res.send(counter.toString());
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname,'ui','article-one.html'))
+var names =[];
+app.get('/submit-name',function (req,res) { //submit-name?name=xxxxx
+  //Get the name from the request
+  var name =  req.query.name;
+  names.push(name);
+  //JSON
+  res.send(JSON.stringify(names));
 });
+
+app.get('/:articleName',function (req,res) {
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+})
+
+// app.get('/article-one',function(req,res)
+// {
+//      //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+//      res.send(createTemplate(articleOne));
+// });
+// app.get('/article-two',function(req,res)
+// {
+//     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+// });
+// app.get('/article-three',function(req,res)
+// {
+//     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+// });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -20,6 +133,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js',function (req,res) {
+  res.sendFile(path.join(__dirname,'ui','main.js'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,77 @@
-console.log('Loaded!');
+
+var button = document.getElementById('counter');
+button.onclick =function() {
+  var request = new XMLHttpRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange =function () {
+    if(request.readyState === XMLHttpRequest.DONE){
+      if(request.status === 200){
+        var counter = request.responseText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+      }
+    }
+  };
+  //make the request
+  request.open('GET','http://localhost:8080/counter',true);
+  request.send(null);
+};
+
+//another practical
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  var request = new XMLHttpRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange =function () {
+    if(request.readyState === XMLHttpRequest.DONE){
+      if(request.status === 200){
+        // var names = ['name1','name2','name3','name4']; instead of hard coding
+        var names = request.responseText;
+        names = JSON.parse(names);
+        var list = '';
+        for(var i=0;i<names.length;i++)
+        {
+         list += '<li>'+names[i] +'</li>' ;
+        }
+
+        var ul = document.getElementById('nameList');
+        ul.innerHTML =list;
+
+        // var counter = request.responseText;
+        // var span = document.getElementById('count');
+        // span.innerHTML = counter.toString();
+      }
+    }
+  };
+  //make the request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  request.open('GET','http://localhost:8080/submit-name?name='+name,true);
+  request.send(null);
+ //make a request to the server and send the name
+
+ //capture a list of names and render it as a list
+}
+
+
+
+// console.log('Loaded!');
+//
+//
+// //Change text
+// var element = document.getElementById('content');
+//
+// console.log(element);
+// element.innerHTML = 'Click on the image to move it left.';
+//
+// var image = document.getElementById('madi');
+// var marginLeft =0;
+// function moveRight() {
+//   marginLeft = marginLeft +5;
+//   image.style.marginLeft = marginLeft + 'px';
+// }
+//
+// image.onclick = function () {
+//   var interval = setInterval(moveRight,50);
+// }
