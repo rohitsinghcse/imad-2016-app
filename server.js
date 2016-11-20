8ea6d977ab5005c056fb4aa2f273fb75f41ff710
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-20 21:49:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto =require('crypto');
 
 
 var config = {
@@ -101,6 +102,17 @@ var counter =0;
 app.get('/counter',function (req,res) {
   counter = counter +1;
   res.send(counter.toString());
+});
+
+function hash(input,salt){
+    //create a hash 
+    var hashed = crypto.pbkdf2(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 var pool= new Pool(config);
