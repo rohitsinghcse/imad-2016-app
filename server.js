3c088cd7d6f54134f8aae4b223a7980694f223e0
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-20 21:56:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/counter',function (req,res) {
 
 function hash(input,salt){
     //create a hash 
-    var hashed = crypto.pbkdf2(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
 }
 
