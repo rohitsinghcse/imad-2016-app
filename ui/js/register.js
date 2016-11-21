1dadae8149576a345dfe79c31582610e6d6dfd64
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-21 16:28:49 +0530

    [imad-console] Updates ui/js/register.js

--- ui/js/register.js
+++ ui/js/register.js
@@ -1,1 +1,28 @@
-
+//registration
+var register = document.getElementById('register_btn');
+register.onclick = function () {
+  var request = new XMLHttpRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange =function () {
+    if(request.readyState === XMLHttpRequest.DONE){
+      if(request.status === 200){
+       console.log('user logged in');
+       alert('Registration successful');
+      }
+      else if (request.status ===500){
+          alert('Something went wrong with the server');
+      }
+    }
+  };
+  
+  //registration module
+  var username = document.getElementById('user').value;
+  var password = document.getElementById('pass').value;
+  console.log(user);
+  console.log(pass);
+   //make the request
+    request.open('POST','http://rohitsinghcse.imad.hasura-app.io/register');
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:"user",password:"pass"}));
+      
+};
