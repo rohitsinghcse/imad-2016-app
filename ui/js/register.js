fc0f297633b4c91de4babc3ba4d9411c0de3a721
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-21 20:32:38 +0530

    [imad-console] Updates ui/js/register.js

--- ui/js/register.js
+++ ui/js/register.js
@@ -1,14 +1,14 @@
 //registration
 var register = document.getElementById('register_btn');
 register.onclick = function () {
-  var request = new XMLHttpRequest();
+  var newRequest = new XMLHttpRequest();
   //capture the response and store it in a variable
-  request.onreadystatechange =function () {
-    if(request.readyState === XMLHttpRequest.DONE){
-      if(request.status === 200){
+  newRequest.onreadystatechange =function () {
+    if(newRequest.readyState === XMLHttpRequest.DONE){
+      if(newRequest.status === 200){
           alert('Registration successful');
       }
-      else if(request.status ===500){
+      else if(newRequest.status ===500){
           alert('Something went wrong with the server');
         }
     }
@@ -20,8 +20,8 @@ register.onclick = function () {
   console.log(newUsername);
   console.log(newPassword);
    //make the request
-    request.open('POST','http://rohitsinghcse.imad.hasura-app.io/create-user');
-    request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({newUsername:newUsername,newPassword:newPassword}));
+    newRequest.open('POST','http://rohitsinghcse.imad.hasura-app.io/create-user');
+    newRequest.setRequestHeader('Content-Type','application/json');
+    newRequest.send(JSON.stringify({newUsername:newUsername,newPassword:newPassword}));
     
 };
