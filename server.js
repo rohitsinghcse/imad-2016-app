32bd5c8ff2e4a5014e5dc9d8ca3a60b2f42c6771
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-21 11:41:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -129,7 +129,38 @@ app.post('/create-user',function(req,res){
           res.status(500).send(err.toString());
       } 
       else{
+          if(results.rows.length===0){
+            res.send(403).send('Username or password is invalid');
+          }
+          else{
+          //Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password,salt);
+              if(hashedPassword === dbString){
+                res.send("Credentials are correct");
+              }
+              else{
+                res.send(403).send('Username or password is invalid');
+              }
+          }
           res.send('User successfully created'+username);
+      }
+    });
+});
+
+app.post('/login',function(req,res){
+    var username = req.body.username; //Request body req.body
+    var password = req.body.password;
+    var dbString = hash(password,salt);
+    pool.query('SELECT * FROM USERS WHERE username =$1)',[username],function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      } 
+      else{
+          if( 
+          res.send('User successfully created'+username);
+          
       }
     });
 });
