816bf4eb3203bb7e805e7173464f353998478168
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-21 15:57:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,10 @@ login.onclick = function () {
       }
     }
   };
- 
+  
+  //registration module
+  
+  
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
   console.log(username);
@@ -50,6 +53,38 @@ login.onclick = function () {
       
 };
 
+
+//registration
+var register = document.getElementById('register_btn');
+register.onclick = function () {
+  var request = new XMLHttpRequest();
+  //capture the response and store it in a variable
+  request.onreadystatechange =function () {
+    if(request.readyState === XMLHttpRequest.DONE){
+      if(request.status === 200){
+       console.log('user logged in');
+       alert('Registration successful');
+      }
+      else if (request.status ===403){
+          alert('username/password is incorrect');
+      }
+      else if (request.status ===500){
+          alert('Something went wrong with the server');
+      }
+    }
+  };
+  
+  //registration module
+  var username = document.getElementById('user').value;
+  var password = document.getElementById('pass').value;
+  console.log(user);
+  console.log(pass);
+   //make the request
+    request.open('POST','http://rohitsinghcse.imad.hasura-app.io/register');
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:"user",password:"pass"}));
+      
+};
 
 //another practical
 
