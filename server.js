62592d4ee8183393f69447ca0b574c460da89476
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-21 11:56:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -152,14 +152,25 @@ app.post('/create-user',function(req,res){
 app.post('/login',function(req,res){
     var username = req.body.username; //Request body req.body
     var password = req.body.password;
-    var dbString = hash(password,salt);
     pool.query('SELECT * FROM USERS WHERE username =$1)',[username],function(err,result){
       if(err){
           res.status(500).send(err.toString());
       } 
-      else{
-          if( 
-          res.send('User successfully created'+username);
+      else {
+          if(result.rows.length ===0){
+              res.send(403).send('Username/password is invalid');        
+      }else{
+        //match the password
+          var dbString =result.rows[0].password;
+          var salt = dbString.split('$')[2];
+          var hashedPassword = hash(password,salt);
+          if(hashedPassword ===dbString){
+            res.send('Logged in ');
+          }else
+          {
+           res.send(403).send('Username/password is invalid');        
+          }
+          
           
       }
     });
