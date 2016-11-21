026b4033616cd1018d8eb161e72f519764b0b851
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-22 00:01:17 +0530

    [imad-console] Updates ui/js/register.js

--- ui/js/register.js
+++ ui/js/register.js
@@ -1,3 +1,31 @@
+// //registration
+// var register = document.getElementById('register_btn');
+// register.onclick = function () {
+//   var newRequest = new XMLHttpRequest();
+//   //capture the response and store it in a variable
+//   newRequest.onreadystatechange =function () {
+//     if(newRequest.readyState === XMLHttpRequest.DONE){
+//       if(newRequest.status === 200){
+//           alert('Registration successful');
+//       }
+//       else if(newRequest.status ===500){
+//           alert('Something went wrong with the server');
+//         }
+//     }
+//   };
+  
+//   //registration module
+//   var newUsername = document.getElementById('user').value;
+//   var newPassword = document.getElementById('pass').value;
+//   console.log(newUsername);
+//   console.log(newPassword);
+//   //make the request
+//     newRequest.open('POST','http://rohitsinghcse.imad.hasura-app.io/create-user');
+//     newRequest.setRequestHeader('Content-Type','application/json');
+//     newRequest.send(JSON.stringify({newUsername:newUsername,newPassword:newPassword}));
+    
+// };
+
 //registration
 var register = document.getElementById('register_btn');
 register.onclick = function () {
@@ -20,9 +48,9 @@ register.onclick = function () {
   console.log(newUsername);
   console.log(newPassword);
    //make the request
-    newRequest.open('POST','http://rohitsinghcse.imad.hasura-app.io/create-user');
+    newRequest.open('POST','http://rohitsinghcse.imad.hasura-app.io/register-user');
     newRequest.setRequestHeader('Content-Type','application/json');
-    newRequest.send(JSON.stringify({newUsername:newUsername,newPassword:newPassword}));
+    newRequest.send(JSON.stringify({uname:newUsername,passwd:newPassword}));
     
 };
 
