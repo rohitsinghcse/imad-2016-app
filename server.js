b7943c09d5c56ca7c2afcee62ca1cbe095d4322f
Er.Rohit Singh <contact.rohit56@gmail.com>
2016-11-21 16:34:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -258,7 +258,7 @@ app.get('/ui/main.js',function (req,res) {
   res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
-app.get('ui/js/register.js',function(req,res){
+app.get('/ui/js/register.js',function(req,res){
     res.sendFile(path.join(__dirname,'ui/js','register.js'));
     
 });
